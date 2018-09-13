956d8ce74c1dce39b0c7078a98ce2e780d69e031
Vicente Soriano <vicente.soriano@icloud.com>
2018-09-13 18:32:06 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,7 +25,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweets)
-  new_tweets = []
+  new_tweets = ""
   tweets.each do |tweet|
     new_tweets << word_substituter(tweet)
   end
