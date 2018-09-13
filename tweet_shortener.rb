84b776e52de3cc910c8f81719035c64487c498b7
Vicente Soriano <vicente.soriano@icloud.com>
2018-09-13 18:23:29 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+def word_substituter(tweet)
+  new_tweet = tweet.split
+  empty_tweet = []
+  new_tweet.collect do |word|
+    case word
+       when "hello"
+        word = "hi"
+       when "to" || "two" || "too"
+        word = "2"
+       when "for" || "four"
+         word = "4"
+       when "be"
+         word = "b"
+       when "you"
+         word = "u"
+       when "at"
+         word = "@"
+       when "and"
+         word = "&"
+     end
+     empty_tweet << word
+  end
+  empty_tweet.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  
+end
+
+def selective_tweet_shortener
+  
+end
+
+def shortened_tweet_truncator
+  
+end
